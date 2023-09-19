047a3e1ec47eedf8e45f5447a333664b9085898a
Valsov <valentin.sovignet@epsi.fr>
2023-09-19 16:22:58 +0200

    Handle variables evaluation

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/valsov/gointerpreter/evaluator"
 	"github.com/valsov/gointerpreter/lexer"
+	"github.com/valsov/gointerpreter/object"
 	"github.com/valsov/gointerpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	environment := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -32,7 +34,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, environment)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
